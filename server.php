256d4558ce69cdde7b359dc533d4733e71d2218a
Adrien AGGERY <adrien@aggery.com>
2015-04-19 17:01:53 +0200

    changed port

--- server.php
+++ server.php
@@ -6,7 +6,7 @@ use Rush01\Lobby;
 require 'vendor/autoload.php';
 require_once('classes/Lobby.class.php');
 
-$server = IoServer::factory(new Lobby(), 1337);
+$server = IoServer::factory(new Lobby(), 8090);
 
 echo "Lobby server running on localhost:1337\n";
 
