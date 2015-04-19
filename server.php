ef261082181d8ed788847f2eed2288cf8b69c598
Adrien AGGERY <adrien@aggery.com>
2015-04-19 17:06:17 +0200

    added http server for connection plus back to port 1337

--- server.php
+++ server.php
@@ -6,7 +6,10 @@ use Rush01\Lobby;
 require 'vendor/autoload.php';
 require_once('classes/Lobby.class.php');
 
-$server = IoServer::factory(new Lobby(), 8090);
+$server = IoServer::factory(
+	new HttpServer(
+		new WsServer(
+			new Lobby())), 1337);
 
 echo "Lobby server running on localhost:1337\n";
 
